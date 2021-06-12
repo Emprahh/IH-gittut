ea5dbfa5564a35e6ff8dbf5fc9bd4c19eca1fe6c
Shaun Reilly <shaundr@hotmail.co.uk>
2021-06-12 10:54:35 +0100

    new line

--- src/com/ironhack/git/main/Git.java
+++ src/com/ironhack/git/main/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("Hello  G it");
         System.out.println("Hello  Git fr om new branch");
+        System.out.println("New Line");
+
     }
 }
