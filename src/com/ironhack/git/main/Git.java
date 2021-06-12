3ff817f65da15197fefac3ba02843988830c8efc
Shaun Reilly <shaundr@hotmail.co.uk>
2021-06-12 10:27:41 +0100

    new branch commit

--- src/com/ironhack/git/main/Git.java
+++ src/com/ironhack/git/main/Git.java
@@ -3,5 +3,6 @@ package com.ironhack.git.main;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello  G it");
+        System.out.println("Hello  Git from new branch");
     }
 }
