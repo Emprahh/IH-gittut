79f5c7d9317232368f200137264f2fc1bcbf48a8
Shaun Reilly <shaundr@hotmail.co.uk>
2021-06-12 10:48:10 +0100

    fixed typo

--- src/com/ironhack/git/main/Git.java
+++ src/com/ironhack/git/main/Git.java
@@ -3,6 +3,6 @@ package com.ironhack.git.main;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello  G it");
-        System.out.println("Hello  Git from new branch");
+        System.out.println("Hello  Git fr om new branch");
     }
 }
